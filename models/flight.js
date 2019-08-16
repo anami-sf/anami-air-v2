7c740c2ffa81ffa0a8b278981e9c1259019e0ffd
anami823 <anami823@gmail.com>
2019-08-15 21:49:24 -0700

    Create mongoose schema, compile into a model and export it

---
+++ models/flight.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+//TODO add data validations
+var flightSchema = new mongoose.Schema({
+    content: String, //enum to include 'American', 'Southwest' & 'United'
+    flightNo: Number, //Required - Between 10 and 9999
+    departs: Date // Default value: One year from date created
+});
+
+module.exports = mongoose.model('Flight', postSchema);
