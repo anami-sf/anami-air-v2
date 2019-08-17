ed4b886a7fe0acbe1ae4296a613791dc3d8dafbd
anami823 <anami823@gmail.com>
2019-08-17 10:33:58 -0700

    revert to master

--- models/flight.js
+++ models/flight.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 //TODO add data validations
 var flightSchema = new mongoose.Schema({
-    content: String, //enum to include 'American', 'Southwest' & 'United'
+    airline: String, //enum to include 'American', 'Southwest' & 'United'
     flightNo: Number, //Required - Between 10 and 9999
     departs: Date // Default value: One year from date created
 });
