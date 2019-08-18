92d20fdc9a206fef8c910795dd7baccdda000e77
anami823 <anami823@gmail.com>
2019-08-18 08:28:37 -0700

    Create subdocument destinationSchema and embed into flightSchema

--- models/flight.js
+++ models/flight.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
 
+var destinationSchema = new mongoose.Schema({
+    airport: {type: String, min: 3, max: 3, default: 3},
+    arrival: Date
+});
+
 //TODO add data validations
 var flightSchema = new mongoose.Schema({
     airline: String, //enum to include 'American', 'Southwest' & 'United'
     flightNo: Number, //Required - Between 10 and 9999
-    departs: Date // Default value: One year from date created
+    departs: Date, // Default value: One year from date created
+    destinations: [destinationSchema]
 });
 
 module.exports = mongoose.model('Flight', flightSchema);
